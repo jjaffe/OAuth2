042074d863c314fb223051815215601945e237e5
Jonathan Jaffe <jjaffe@thirdrating.com>
2014-03-30 18:34:57 -0400

    update to add BNCClient

---
+++ OAuth2/Client/Impl/BNCClient.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+
+namespace OAuth2.Client.Impl
+{
+    /// <summary>
+    /// BNC authentication client.
+    /// </summary>
+    public class BNCClient : OAuth2Client
+    {
+        public BNCClient(IRequestFactory factory, IClientConfiguration configuration)
+            : base(factory, configuration)
+        {
+        }
+
+        protected override void BeforeGetAccessToken(BeforeAfterRequestArgs args)
+        {
+            args.Request.AddObject(new
+            {
+                code = args.Parameters.GetOrThrowUnexpectedResponse("code"),
+                client_id = args.Configuration.ClientId,
+                client_secret = args.Configuration.ClientSecret,
+                redirect_uri = args.Configuration.RedirectUri,
+                state = State,
+                grant_type = "authorization_code"
+            });
+        }
+
+        /// <summary>
+        /// Should return parsed <see cref="UserInfo"/> from content received from third-party service.
+        /// </summary>
+        /// <param name="content">The content which is received from third-party service.</param>
+        protected override UserInfo ParseUserInfo(string content)
+        {
+            /*
+            var cnt = JObject.Parse(content);
+            var names = cnt["name"].Value<string>().Split(' ').ToList();
+            const string avatarUriTemplate = "{0}&s={1}";
+            var avatarUri = cnt["avatar_url"].Value<string>();
+            var result = new UserInfo
+                {
+                    Email = cnt["email"].SafeGet(x => x.Value<string>()),
+                    ProviderName = this.Name,
+                    Id = cnt["id"].Value<string>(),
+                    FirstName = names.Count > 0 ? names.First() : cnt["login"].Value<string>(),
+                    LastName = names.Count > 1 ? names.Last() : string.Empty,
+                    AvatarUri =
+                        {
+                            Small = !string.IsNullOrWhiteSpace(avatarUri) ? string.Format(avatarUriTemplate, avatarUri, AvatarInfo.SmallSize) : string.Empty,
+                            Normal = avatarUri,
+                            Large = !string.IsNullOrWhiteSpace(avatarUri) ? string.Format(avatarUriTemplate, avatarUri, AvatarInfo.LargeSize) : string.Empty
+                        }
+                };
+            return result;*/
+            return new UserInfo();
+        }
+
+        /// <summary>
+        /// Friendly name of provider (OAuth2 service).
+        /// </summary>
+        public override string Name
+        {
+            get { return "BNC"; }
+        }
+
+        /// <summary>
+        /// Defines URI of service which issues access code.
+        /// </summary>
+        protected override Endpoint AccessCodeServiceEndpoint
+        {
+            get { return new Endpoint { BaseUri = "http://bncauth-dev.herokuapp.com", Resource = "/oauth/authorize" }; }
+        }
+
+        /// <summary>
+        /// Defines URI of service which issues access token.
+        /// </summary>
+        protected override Endpoint AccessTokenServiceEndpoint
+        {
+            get { return new Endpoint { BaseUri = "http://bncauth-dev.herokuapp.com", Resource = "/oauth/token" }; }
+        }
+
+        /// <summary>
+        /// Defines URI of service which allows to obtain information about user which is currently logged in.
+        /// </summary>
+        protected override Endpoint UserInfoServiceEndpoint
+        {
+            get { return new Endpoint { BaseUri = "http://bncauth-dev.herokuapp.com", Resource = "/api/me" }; }
+        }
+    }
+}
--- OAuth2/Infrastructure/RestClientExtensions.cs
+++ OAuth2/Infrastructure/RestClientExtensions.cs
@@ -8,6 +8,8 @@ namespace OAuth2.Infrastructure
     {
         public static IRestResponse ExecuteAndVerify(this IRestClient client, IRestRequest request)
         {
+            // add header
+            //request.AddHeader("X-Authorization-Key", "5ed2149a-e2ba-4a40-9df0-94057d1942fb");
             var response = client.Execute(request);
             if (response.StatusCode != HttpStatusCode.OK ||
                 response.Content.IsEmpty())
